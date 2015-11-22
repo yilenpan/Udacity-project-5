e5556dd844ce136c20e417b9852468994808e75a
Yilen Pan <yilen.pan@gmail.com>
2015-11-21 19:40:57 -0800

    Update my.js

--- js/my.js
+++ js/my.js
@@ -1,9 +1,6 @@
 /*
 I'll Find It!
 
-Udacity Frontend Nanodegree Project 5, Neighborhood Map
-By Yilen Pan
-
 Insert a search term and an address, and it will return a list of search items
 puts them on the map and on the side menu.
 
